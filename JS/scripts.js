739847042a569500a4292c3d418aa9d454a851f7
matthewslape <75864370+matthewslape@users.noreply.github.com>
2021-03-10 13:49:13 -0800

    fixed javascript

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,14 @@
 console.log("OK");
 
+//call lightgallery
+const portfolio = document.getElementsByClassName("portfolio");
+
+lightGallery(portfolio,  {
+
+	mode: "lg-zoom-in-big"
+	
+});
+
 // Store Background Classes
 const backgrounds = ["bg0", "bg1", "bg2", "bg3", "bg4"];
 
@@ -12,13 +21,3 @@ let pageBackground = document.querySelector("body");
 
 // Set class name from stored values
 pageBackground.className = backgrounds[numRand];
-
-
-//call lightgallery
-const portfolio = document.getElementsByClassName("portfolio");
-
-lightGallery(portfolio,  {
-
-	mode: "lg-zoom-in-big"
-	
-});
