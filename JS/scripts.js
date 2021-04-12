03d104dc1498eeb9c607d32697d8fff4c41a0053
matthewslape <75864370+matthewslape@users.noreply.github.com>
2021-04-12 11:54:56 -0700

    updated nav and body bg

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,14 +1,2 @@
 console.log("Test");
 
-// Store Background Classes
-const backgrounds = ["bg0", "bg1", "bg2", "bg3", "bg4"];
-
-// Get Random Number
-var numRand;
-numRand = Math.floor(Math.random() * backgrounds.length);
-
-// Get element
-let pageBackground = document.querySelector("body");
-
-// Set class name from stored values
-pageBackground.className = backgrounds[numRand];
